e5780d7def2382c3060d543898a3b7aa66228806
Cobalt <c0balt@disroot.org>
2022-01-18 11:52:38 +0100

    format + cleanup merge duplicates

--- tests/util.rs
+++ tests/util.rs
@@ -5,14 +5,14 @@ use deno_core::error::JsError;
 use js_sandbox::AnyError;
 
 pub fn expect_error<T>(result: Result<T, AnyError>, error_type: &str) {
-    let err = match result {
-        Ok(_) => panic!("Call with {} must not succeed", error_type),
-        Err(e) => e,
-    };
+	let err = match result {
+		Ok(_) => panic!("Call with {} must not succeed", error_type),
+		Err(e) => e,
+	};
 
-    let err = err
-        .downcast_ref::<JsError>()
-        .expect(&format!("{} must lead to JsError type", error_type));
+	let err = err
+		.downcast_ref::<JsError>()
+		.expect(&format!("{} must lead to JsError type", error_type));
 
-    println!("Expected error occurred:\n{}", err.message);
+	println!("Expected error occurred:\n{}", err.message);
 }
